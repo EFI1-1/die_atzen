a8c318d4eda0a15108e5414271310b6c5200b056
Nico | DarkPlugins <Kirchnern15@s.bbst-clp.de>
2025-05-22 14:20:12 +0200

    Added new functions for DBManager, tested DBManager with dummy values

---
+++ test.py
@@ -0,0 +1,46 @@
+from Backend.Utils.DBManager import DBManager
+from Backend.Value import Value
+from Backend.ProgramSettings import ProgramSettings
+
+### Testing ###
+
+# Create database
+db = DBManager()
+
+# Update settings
+# Get users saved password from db
+user_enc_password = db.GetSave(1)
+# decrypt saved password and use default encryption value
+#ProgramSettings.CRYPTKEY = ProgramSettings.DEFAULT_CRYPT_KEY
+#user_enc_password = encryption.Decrypt(user_enc_password)
+# check entered password with user_enc_password
+#if enteredPassword == user_enc_password:
+#    # Set crypt key to user_enc_password:
+#    ProgramSettings.CRYPT_KEY = user_enc_password
+#else:
+#    print("Does not match")
+
+# Create value
+someValue = Value(0, "Test", "Something")
+
+# Add value to database and check result, override id
+result = db.CreateValue(someValue)
+if result > 0:
+    someValue.id = result
+
+# Change and save value
+someValue.name = "A changed name"
+someValue.key = "A changed key"
+if not db.SaveValue(someValue):
+    print("An error occured saving the value in the database")
+
+# Give out all values in db
+values = db.GetAllValues()
+for v in values:
+    print(f"ID: {v.id}, Name: {v.name}, Key: {v.key}")
+
+# Delete value
+if db.DeleteValue(someValue):
+    print("Value deleted")
+else:
+    print("An error occured deleting the value")
